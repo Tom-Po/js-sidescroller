30863b0c727c1458f654c9832a47b9a08b7d611b
Tom Poidevin <tom.poidevin@gmail.com>
2023-04-21 17:26:25 +0200

    eslint

--- entity/sprite.js
+++ entity/sprite.js
@@ -11,6 +11,12 @@ export default class Sprite {
       context.strokeStyle = 'red';
       context.strokeRect(this.x, this.y, this.spriteWidth, this.spriteHeight);
     }
-    context.drawImage(this.image, 800 - this.width * this.scale, 600 - 100, this.width * this.scale, this.height * this.scale);
+    context.drawImage(
+      this.image,
+      800 - this.width * this.scale,
+      600 - 100,
+      this.width * this.scale,
+      this.height * this.scale,
+    );
   }
 }
